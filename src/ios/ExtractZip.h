3926d5d5c160c2d6aff9303cc27df73b59775727
Evgeniy Lukovsky <strong.drug@gmail.com>
2013-10-04 17:22:06 +0400

    Added iOS support.

---
+++ src/ios/ExtractZip.h
@@ -0,0 +1,19 @@
+//
+//  ExtractZipFilePlugin.h
+//
+//  Created by Shaun Rowe on 10/05/2012.
+//  Copyright (c) 2012 Pobl Creative Cyf. All rights reserved.
+//
+
+#import <Cordova/CDV.h>
+#import "SSZipArchive/SSZipArchive.h"
+
+@interface ExtractZip : CDVPlugin
+{
+    NSString *callbackID;
+}
+
+- (void)extract:(CDVInvokedUrlCommand*)command;
+- (void)getTempDir:(CDVInvokedUrlCommand*)command;
+
+@end
---
+++ src/ios/ExtractZip.m
@@ -0,0 +1,55 @@
+//
+//  ExtractZipFilePlugin.m
+//
+//  Created by Shaun Rowe on 10/05/2012.
+//  Copyright (c) 2012 Pobl Creative Cyf. All rights reserved.
+//
+
+#import "ExtractZip.h"
+#import "SSZipArchive/SSZipArchive.h"
+
+@implementation ExtractZip
+
+
+- (void)extract:(CDVInvokedUrlCommand *)command
+{
+    NSArray* arguments = command.arguments;
+    
+    NSLog(@"Extract zip has been started.");
+    NSString *file = [arguments objectAtIndex:0];
+    NSString *destination = [arguments objectAtIndex:1];
+
+    CDVPluginResult *result;
+    if([SSZipArchive unzipFileAtPath:file toDestination:destination delegate:nil]) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[destination stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        [self writeJavascript:[result toSuccessCallbackString:callbackID]];
+        NSLog(@"Success!");
+    } else {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[@"Could not extract archive" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+        [self writeJavascript:[result toErrorCallbackString:command.callbackId]];
+        NSLog(@"Error!");
+    }
+}
+
+- (void)getTempDir:(CDVInvokedUrlCommand *)command
+{
+    CDVPluginResult * pluginResult = nil;
+    NSError* error = nil;
+    
+    NSString* dirName = [command.arguments objectAtIndex:0];
+    NSString* path = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString* dirPath = [path stringByAppendingPathComponent:dirName];
+    
+    NSLog(@"Trying to create directory: %@",dirName);
+    bool isCreated = ![[NSFileManager defaultManager] createDirectoryAtPath:dirPath withIntermediateDirectories:NO attributes:nil error: &error];
+    
+    if (isCreated) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:dirPath];
+        NSLog(@"Success!");
+    }else{
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Can't create the directory."];
+    }
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
